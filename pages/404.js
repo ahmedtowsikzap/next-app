d63515c34d7f36ac796f554a8f508da0937ffe73
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-04-25 00:35:42 +0600

    custom 404 useRouter to navigate

---
+++ pages/404.js
@@ -0,0 +1,4 @@
+function PageNotFound()=>[
+   return <h1>404 page will all custom styling neccessary</h1>
+]
+export default PageNotFound
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
 import Link from "next/link"
+import { useRouter } from "next/router"
 
 function Home(){
+    const router = useRouter()
+     
+    const handleClick =(e)=>{
+         console.log('placing your order')
+         router.push('/product')
+
+    }
     return (
     <div>
      <h1>Home</h1>
@@ -10,6 +18,9 @@ function Home(){
      <Link href="/product">
      <a>products</a>
      </Link>
+     <button onClick={handleClick}>
+         placeorder
+     </button>
     </div>
     )
 }
